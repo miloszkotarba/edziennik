0d386c17f8b289ec685173cd9d515075ee435450
Miłosz Kotarba <126890720+Piotrekk00@users.noreply.github.com>
2023-04-13 18:11:53 +0200

    Zmiana języka na PL

--- app/Page.php
+++ app/Page.php
@@ -7,7 +7,7 @@ class Page
     {
         ?>
         <!DOCTYPE html>
-        <html lang="en">
+        <html lang="pl-PL">
         <head>
             <meta charset="UTF-8"/>
             <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
