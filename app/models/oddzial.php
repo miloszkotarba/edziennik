030cea3a7803937648e2e9e733aa47602bd11d82
Miłosz Kotarba <111527970+miloszkotarba@users.noreply.github.com>
2023-05-31 21:35:21 +0200

    Wyswietlanie potencjalnych wychowawcow przy edycji klasy

--- app/models/oddzial.php
+++ app/models/oddzial.php
@@ -24,6 +24,24 @@ class Oddzialy
         }
     }
 
+    public function showNotWychowawcy2($klasaId)
+    {
+        $this->db->query('SELECT u . usersId, u . usersName, u . usersSurname FROM users u LEFT JOIN klasa k ON u . usersId = k . tutorId WHERE usersRole = 1 and k . tutorId IS NULL                                                                                                           UNION ALL
+SELECT (SELECT usersId FROM users WHERE usersId = k.tutorId), (SELECT usersName FROM users WHERE usersId = k.tutorId), (SELECT usersSurname FROM users WHERE usersId = k.tutorId)
+FROM klasa k
+WHERE k.klasaId = :klasaId
+ORDER BY 3,2;');
+
+        $this->db->bind(':klasaId', $klasaId);
+        $row = $this->db->resultSet();
+
+        if ($this->db->rowCount() > 0) {
+            return $row;
+        } else {
+            return false;
+        }
+    }
+
     public function checkIfNotWychowawca($usersId)
     {
         $this->db->query('SELECT u . usersId, u . usersName, u . usersSurname FROM users u LEFT JOIN klasa k ON u . usersId = k . tutorId WHERE usersRole = 1 and k . tutorId IS NULL AND u.usersId = :usersId ORDER BY u.usersSurname, u.usersName;');
@@ -62,6 +80,20 @@ class Oddzialy
         }
     }
 
+    public function checkOddzialUnique2($klasaName, $klasaId)
+    {
+        $this->db->query('SELECT * FROM klasa WHERE klasaName = :klasaName AND klasaId != :klasaId');
+        $this->db->bind(':klasaName', $klasaName);
+        $this->db->bind(':klasaId', $klasaId);
+
+        $final = $this->db->execute();
+        if ($this->db->rowCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
     public function checkOddzialUniqueId($klasaId)
     {
@@ -70,7 +102,7 @@ class Oddzialy
 
         $final = $this->db->resultSet();
         if ($this->db->rowCount() > 0) {
-            return true;
+            return $final;
         } else {
             return false;
         }
@@ -89,7 +121,8 @@ class Oddzialy
         }
     }
 
-    public function checkKlasaId($klasaName) {
+    public function checkKlasaId($klasaName)
+    {
         $this->db->query('SELECT klasaId FROM klasa WHERE klasaName = :klasaName');
         $this->db->bind(':klasaName', $klasaName);
         $final = $this->db->resultSet();
@@ -117,7 +150,7 @@ ORDER BY k.klasaName;');
     {
         $this->db->query('SELECT YearId FROM SchoolYear ORDER BY semestr2do DESC LIMIT 1');
         $row = $this->db->resultSet();
-        if($row) return $row;
+        if ($row) return $row;
         else return false;
     }
 
@@ -132,11 +165,12 @@ ORDER BY k.klasaName;');
         else return false;
     }
 
-    public function showOddzialName($klasaId) {
+    public function showOddzialName($klasaId)
+    {
         $this->db->query('SELECT klasaName FROM klasa WHERE klasaId = :klasaId');
         $this->db->bind(':klasaId', $klasaId);
         $row = $this->db->resultSet();
-        if($row) return $row;
+        if ($row) return $row;
         else return false;
     }
 
@@ -154,5 +188,42 @@ ORDER BY usersSurname, usersName;');
         } else {
             return false;
         }
+    }
+
+    public function deleteOddzial($klasaId)
+    {
+        try {
+            $this->db->query('DELETE FROM Oddzialy WHERE klasaId = :klasaId');
+            $this->db->bind(':klasaId', $klasaId);
+
+            $row = $this->db->execute();
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    public function deleteKlasa($klasaId)
+    {
+        try {
+            $this->db->query('DELETE FROM klasa WHERE klasaId = :klasaId');
+            $this->db->bind(':klasaId', $klasaId);
+
+            $row = $this->db->execute();
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    public function updateKlasa($klasaId, $klasaName, $tutorId) {
+        $this->db->query('UPDATE klasa SET klasaName = :klasaName, tutorId = :tutorId WHERE klasaId = :klasaId');
+        $this->db->bind(':klasaId', $klasaId);
+        $this->db->bind(':klasaName', $klasaName);
+        $this->db->bind(':tutorId', $tutorId);
+
+        $row = $this->db->execute();
+        if($this -> db ->rowCount() > 0) return $row;
+        else return false;
     }
 }
