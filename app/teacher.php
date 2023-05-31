3b10397e8572d19946698761c7f378c6887c4a42
Miłosz Kotarba <111527970+miloszkotarba@users.noreply.github.com>
2023-05-31 21:36:15 +0200

    Usuwanie i edycja nauczyciela

--- app/teacher.php
+++ app/teacher.php
@@ -71,8 +71,8 @@ class Teacher extends Controller
         }
 
         $email = $this->Model->checkUserEmail($data['email']);
-        if($email == true) {
-            alerts::SetError("Użytkownik o takim adresie e-mail już istnieje.",0);
+        if ($email == true) {
+            alerts::SetError("Użytkownik o takim adresie e-mail już istnieje.", 0);
             redirect('/teacher/add');
         }
 
@@ -108,12 +108,12 @@ class Teacher extends Controller
 
             ///dodanie kategorii predefiniowanych
             $newTeacherId = $this->Model->showUserId($data['email']);
-            $newTeacherId = $newTeacherId -> usersId;
+            $newTeacherId = $newTeacherId->usersId;
 
-            $this->KategorieModel->insertCategory("przewidywana śródroczna",0,"b0c4de", 0,$newTeacherId);
-            $this->KategorieModel->insertCategory("śródroczna",0,"b0c4de", 0,$newTeacherId);
-            $this->KategorieModel->insertCategory("przewidywana roczna",0,"87cefa", 0,$newTeacherId);
-            $this->KategorieModel->insertCategory("roczna",0,"87cefa", 0,$newTeacherId);
+            $this->KategorieModel->insertCategory("przewidywana śródroczna", 0, "b0c4de", 0, $newTeacherId);
+            $this->KategorieModel->insertCategory("śródroczna", 0, "b0c4de", 0, $newTeacherId);
+            $this->KategorieModel->insertCategory("przewidywana roczna", 0, "87cefa", 0, $newTeacherId);
+            $this->KategorieModel->insertCategory("roczna", 0, "87cefa", 0, $newTeacherId);
 
 
             alerts::SetSuccess("Dodano nauczyciela.");
@@ -135,6 +135,129 @@ class Teacher extends Controller
         if ($_SESSION['usersRole'] != 2) {
             header('Location: /dashboard');
             exit();
+        }
+    }
+
+    public function delete($link = NULL)
+    {
+        $this -> is_admin();
+        if ($link == NULL) {
+            redirect('/teacher');
+        }
+
+        $teacherId = $link;
+
+        $warunek = $this->Model->checkIfTeacherCanBeDeleted($teacherId);
+        if ($warunek) {
+            $final = $this->KategorieModel->deleteAllCategories($teacherId);
+        }
+
+        $final2 = $this->Model->deleteTeacher($teacherId);
+
+        if (!$final2) {
+            alerts::SetError("Błąd. Nie można usunąć przypisanego nauczyciela.");
+            redirect('/teacher');
+        } else {
+            alerts::SetSuccess("Usunięto nauczyciela.");
+            redirect('/teacher');
+        }
+    }
+
+    public function edit($link = NULL)
+    {
+        $this->is_admin();
+
+        if ($link == NULL) {
+            redirect('/teacher');
+        }
+
+        $final = $this->Model->getTeacherById($link);
+        if (!$final) {
+            redirect('/teacher');
+        }
+
+        require 'views/admin/teacher/teacher.edit.php';
+    }
+
+    public function edytuj()
+    {
+        $this->is_admin();
+        //Sanitize POST data
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+        //Init data
+        $data = [
+            'name' => trim($_POST['name']),
+            'surname' => trim($_POST['surname']),
+            'email' => trim($_POST['email']),
+            'login' => trim($_POST['login']),
+            'password' => trim($_POST['password']),
+            'usersId' => trim($_POST['usersId'])
+        ];
+
+
+        $link = '/teacher/edit/' . $data['usersId'];
+
+        //Validate inputs
+        if (empty($data['name']) || empty($data['surname']) || empty($data['email']) || empty($data['login']) || empty($data['password'])) {
+            alerts::SetError("Uzupełnij wszystkie pola.", 0);
+            redirect($link);
+        }
+
+        if (strlen($data['name']) > 60) {
+            alerts::SetError("Imię nie może być dłuższe niż 60 znaków.", 0);
+            redirect($link);
+        }
+
+        $condition = '/(*UTF8)^[A-ZŁŚ]{1}+[a-ząęółśżźćń]+$/';
+        if (!preg_match($condition, $data['name'])) {
+            alerts::SetError("Podano nieprawidłowe imię.", 0);
+            redirect($link);
+        }
+
+        $condition = '/(*UTF8)^[A-ZŁŚ]{1}+[a-ząęółśżźćń]+$/';
+        if (!preg_match($condition, $data['surname'])) {
+            alerts::SetError("Podano nieprawidłowe nazwisko.", 0);
+            redirect($link);
+        }
+
+        if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+            alerts::SetError("Podano nieprawidłowy adres e-mail.", 0);
+            redirect($link);
+        }
+
+
+        if (strlen($data['login']) < 5) {
+            alerts::SetError("Login musi mieć co najmniej 5 znaków.", 0);
+            redirect($link);
+        }
+
+
+        if (strlen($data['login']) > 50) {
+            alerts::SetError("Login nie może być dłuższy niż 50 znaków.", 0);
+            redirect($link);
+        }
+
+        if (strlen($data['password']) < 5) {
+            alerts::SetError("Hasło musi mieć co najmniej 5 znaków.", 0);
+            redirect($link);
+        }
+
+
+        if (strlen($data['password']) > 50) {
+            alerts::SetError("Hasło nie może być dłuższe niż 50 znaków.", 0);
+            redirect($link);
+        }
+
+        $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+        $final = $this->Model->modifyTeacher($data['usersId'], $data['name'], $data['surname'], $data['email'], $data['login'], $data['password']);
+        if (!$final) {
+            alerts::SetError("Błąd: Nie zmodyfikowano nauczyciela.");
+            redirect('/teacher');
+        } else {
+            alerts::SetSuccess("Zmodyfikowano konto nauczyciela.");
+            redirect('/teacher');
         }
     }
 }
