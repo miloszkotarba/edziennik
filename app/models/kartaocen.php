9daa17afed6e5259dd1d6a22ea2dd1caa4075a98
Miłosz Kotarba <126890720+Piotrekk00@users.noreply.github.com>
2023-06-01 21:29:17 +0200

    Dodanie modelu karty ocen

---
+++ app/models/kartaocen.php
@@ -0,0 +1,93 @@
+<?php
+
+require_once 'app/database.php';
+
+class KartyOcen
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    public function checkWychowawstwo($usersId)
+    {
+        $this->db->query('SELECT usersId, usersName, usersSurname, k.klasaName, k.klasaId FROM users u JOIN klasa k ON k.tutorId = u.usersId WHERE usersId = :usersId;');
+
+        $this->db->bind(':usersId', $usersId);
+
+        $row = $this->db->single();
+
+        if ($this->db->rowCount() > 0) {
+            return $row;
+        } else return false;
+    }
+
+    public function showLessonsforStudent($studentId)
+    {
+        $this->db->query('SELECT z.zajeciaId, s.subjectName FROM Zajecia z JOIN subjects s ON s.subjectId = z.subjectId WHERE z.oddzialId = (SELECT oddzialId FROM Oddzialy WHERE klasaId = (SElECT klasaId FROM users WHERE usersId = :studentId)) ORDER BY s.subjectName;');
+
+        $this->db->bind(':studentId', $studentId);
+
+        $result = $this->db->resultSet();
+        if ($result) return $result;
+        else return false;
+    }
+
+    public function showGradesForEachSubject($studentId, $zajeciaId)
+    {
+        $this->db->query('SELECT o.zajeciaId, o.ocenaId, o.value, CONCAT("#",c.color) color, c.weight, c.averageCount, c.name, o.comment, DATE_FORMAT(o.date, "%d-%m-%Y") date, u.usersName, u.usersSurname FROM Oceny o JOIN categories c ON c.categoryId = o.categoryId JOIN zajecia z ON z.zajeciaId = o.zajeciaId JOIN users u ON u.usersId = z.teacherId WHERE o.studentId = :studentId AND c.name != "przewidywana śródroczna" AND c.name != "śródroczna" AND c.name != "przewidywana roczna" AND c.name != "roczna" AND o.zajeciaId = :zajeciaId AND ( o.date>=(SELECT semestr1od FROM SchoolYear ORDER BY YearId DESC LIMIT 1)  AND o.date<=(SELECT semestr1do FROM SchoolYear ORDER BY YearId DESC LIMIT 1)) ORDER BY o.date;');
+
+        $this->db->bind(':studentId', $studentId);
+        $this->db->bind(':zajeciaId', $zajeciaId);
+
+        $result = $this->db->resultSet();
+
+        if ($this->db->rowCount() > 0) {
+            return $result;
+        } else {
+            return false;
+        }
+    }
+
+    public function showUserFromClass($klasaId)
+    {
+        $this->db->query('SELECT usersId, usersName, usersSurname, (SELECT klasaName FROM klasa WHERE klasaId = :klasaId) klasa
+FROM users WHERE klasaId = :klasaId ORDER BY 3,2;');
+
+        $this->db->bind(':klasaId', $klasaId);
+
+        $result = $this->db->resultSet();
+
+        if ($this->db->rowCount() > 0) {
+            return $result;
+        } else {
+            return false;
+        }
+    }
+
+    public function SredniaSem1($zajeciaId, $studentId)
+    {
+        $this->db->query('WITH widok AS(SELECT o.zajeciaId, o.ocenaId, o.value, CONCAT("#",c.color) color, c.weight, c.averageCount, c.name, o.comment, DATE_FORMAT(o.date, "%d-%m-%Y") date, u.usersName, u.usersSurname FROM Oceny o JOIN categories c ON c.categoryId = o.categoryId JOIN Zajecia z ON z.zajeciaId = o.zajeciaId JOIN users u ON u.usersId = z.teacherId WHERE o.studentId = :studentId AND c.name != "przewidywana śródroczna" AND c.name != "śródroczna" AND c.name != "przewidywana roczna" AND c.name != "roczna" AND o.zajeciaId = :zajeciaId AND ( o.date>=(SELECT semestr1od FROM SchoolYear ORDER BY YearId DESC LIMIT 1)  AND o.date<=(SELECT semestr1do FROM SchoolYear ORDER BY YearId DESC LIMIT 1)) AND o.value NOT IN("np","bz","+","-") ORDER BY o.date)
+SELECT ROUND((SUM(value*weight)/SUM(weight)),2) srednia FROM widok;');
+        $this->db->bind(':zajeciaId',$zajeciaId);
+        $this->db->bind(":studentId",$studentId);
+
+        $result = $this->db->single();
+
+        return $result;
+    }
+
+    public function SredniaSem2($zajeciaId, $studentId)
+    {
+        $this->db->query('WITH widok AS(SELECT o.zajeciaId, o.ocenaId, o.value, CONCAT("#",c.color) color, c.weight, c.averageCount, c.name, o.comment, DATE_FORMAT(o.date, "%d-%m-%Y") date, u.usersName, u.usersSurname FROM Oceny o JOIN categories c ON c.categoryId = o.categoryId JOIN Zajecia z ON z.zajeciaId = o.zajeciaId JOIN users u ON u.usersId = z.teacherId WHERE o.studentId = :studentId AND c.name != "przewidywana śródroczna" AND c.name != "śródroczna" AND c.name != "przewidywana roczna" AND c.name != "roczna" AND o.zajeciaId = :zajeciaId AND ( o.date>=(SELECT semestr2od FROM SchoolYear ORDER BY YearId DESC LIMIT 1)  AND o.date<=(SELECT semestr2do FROM SchoolYear ORDER BY YearId DESC LIMIT 1)) AND o.value NOT IN("np","bz","+","-") ORDER BY o.date)
+SELECT ROUND((SUM(value*weight)/SUM(weight)),2) srednia FROM widok;');
+        $this->db->bind(':zajeciaId',$zajeciaId);
+        $this->db->bind(":studentId",$studentId);
+
+        $result = $this->db->single();
+
+        return $result;
+    }
+}
